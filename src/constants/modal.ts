b451e8ae8a1224742fffb507b5f884f27e0d9d91
amirnasr <amir.nasr72@gmail.com>
2023-06-18 16:34:24 +0330

    add 2 interface

--- src/constants/modal.ts
+++ src/constants/modal.ts
@@ -4,9 +4,16 @@ export type Product = {
     price:number,
     description:string,
     sticker:string,
+    cartQty:number
 }
 
 export interface ProductTools {
     status: 'idle' | 'pending' | 'succeeded' | 'failed',
     error: string | undefined
+}
+
+export interface CartProducts {
+    cartproduct: Product[],
+    cartTotalQty:number,
+    cartTotalAmount:number
 }
