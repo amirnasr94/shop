2f9f83a62fefb680693d5785338ce88cb8d28916
amirnasr <amir.nasr72@gmail.com>
2023-06-18 19:39:31 +0330

    implement Navbar & ShopHeader component

---
+++ src/components/ShopHeader.tsx
@@ -0,0 +1,12 @@
+const ShopHeader = () => {
+  return (
+    <div className="mt-16">
+        <h1 className="leading-relaxed font-extrabold text-4xl text-center text-purple-600 mt-4 py-2 sm:py-4">
+            Shopping
+            <p className="max-w-xl text-center px-2 mx-auto text-base text-gray-600">😎</p>
+        </h1>
+    </div>
+  )
+}
+
+export default ShopHeader
