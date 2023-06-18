f8672e02760d612a56fa7f337013d20867d09cb8
amirnasr <amir.nasr72@gmail.com>
2023-06-18 16:33:04 +0330

    implement cartSlice

---
+++ src/redux/reduxReducer/cartSlice.ts
@@ -0,0 +1,50 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { CartProducts,Product } from '../../constants/modal';
+
+const initialState:CartProducts = {
+    cartproduct: localStorage.getItem("cartProduct") ? JSON.parse(localStorage.getItem("cartProduct") || "[]") : [],
+    cartTotalQty:0,
+    cartTotalAmount:0
+}
+
+const cartSlice = createSlice({
+    name:"cart",
+    initialState,
+    reducers:{
+        addToCart(state,action){
+            const exsIndexProduct:number = state.cartproduct.findIndex((item) => item.id === action.payload.id);
+            if (exsIndexProduct >= 0) {
+                state.cartproduct[exsIndexProduct] = {
+                    ...state.cartproduct[exsIndexProduct],
+                    cartQty: state.cartproduct[exsIndexProduct].cartQty + 1
+                }
+            }else{
+                const tepProduct:Product = {
+                    ...action.payload,
+                    cartQty:action.payload.cartQty
+                }
+                state.cartproduct.push(tepProduct);
+            }
+            localStorage.setItem("cartProduct",JSON.stringify(state.cartproduct));
+        },
+        computedTotal(state,action){
+            // eslint-disable-next-line prefer-const
+            let {total,qty} = state.cartproduct.reduce((cartTotal,cartItem) => {
+                const {price,cartQty} = cartItem;
+                const itemTotal = price * cartQty;
+                cartTotal.total += itemTotal;
+                cartTotal.qty += cartQty
+                return cartTotal
+            },{
+                total:0,
+                qty:0
+            })
+            total = parseFloat(total.toFixed());
+            state.cartTotalQty = qty;
+            state.cartTotalAmount = total;
+        }
+    }
+})
+
+export default cartSlice.reducer;
+export const {addToCart,computedTotal} = cartSlice.actions;
