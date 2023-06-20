ce99477dccda7427ab343eb0905979409fd704b9
amirnasr <amir.nasr72@gmail.com>
2023-06-20 10:16:18 +0330

    implement Shop.tsx

---
+++ src/components/Shop.tsx
@@ -0,0 +1,42 @@
+import { useAppSelector } from "../hooks/customHooks";
+import { selectAll } from "../redux/reduxReducer/productSlice"
+import SingleProduct from "./Product";
+
+const Shop = () => {
+    const allProduct = useAppSelector(selectAll);
+    const status = useAppSelector(state => state.products.status);
+
+  return (
+    <section className="w-full h-full">
+        <div></div>
+        <div className="flex">
+            <aside className="w-1/4 bg-slate-500">asd</aside>
+            <article className="w-3/4 bg-orange-300">
+                {
+                    status === "pending" ? (
+                        <div>
+
+                        </div>
+                    ) : status === "succeeded" ? (
+                        <div className="bg-white">
+                            <div className="mx-auto max-w-2xl px-4 py-6 sm:px-6 lg:max-w-7xl lg:px-8">
+                                <h2 className="text-2xl font-bold tracking-tight text-gray-900">Customers also purchased</h2>
+                            </div>
+                            <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8 p-6">
+                            {allProduct.map((product,index) => (
+                                <SingleProduct key={index} product={product}/>
+                            ))}
+                            </div>
+                        </div>
+
+                    ) : (
+                        <div></div>
+                    )
+                }
+            </article>
+        </div>
+    </section>
+  )
+}
+
+export default Shop
