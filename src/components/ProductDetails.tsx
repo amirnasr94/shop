da35ae727ca2e2765e5c42a6ca7439605bbe5e0c
amirnasr <amir.nasr72@gmail.com>
2023-07-18 22:10:20 +0330

    change structure of ProductDetails.tsx

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import {useNavigate, useParams} from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../hooks/customHooks";
 import { selectById } from "../redux/reduxReducer/productSlice";
@@ -5,8 +6,13 @@ import { Helmet } from "react-helmet";
 import {Link} from 'react-router-dom';
 import { Product } from "../constants/modal";
 import { addToCart } from "../redux/reduxReducer/cartSlice";
+import AdditionalDetails from './productExtra/AdditionalDetails';
+import TechnicalDetails from './productExtra/TechnicalDetails';
 
 const ProductDetails = () => {
+  const [showAdditionalDetails,setShowAdditionalDetails] = useState<boolean>(false);
+  const [showTechnicalDeatils,setShowTechnicalDeatils] = useState<boolean>(false);
+
   const {productID} = useParams();
   
   const product = useAppSelector(state => selectById(state,productID as string))
@@ -17,62 +23,50 @@ const ProductDetails = () => {
       dispatch(addToCart(item));
       navigate("/cart")
   };
-  console.log(product);
   
   return (
-    <section className="w-full h-full relative mt-20">
+    <section className="relative">
       {product ? (
         <>
           <Helmet>
             <title>{product.name}</title>
           </Helmet>
-          <section className="grid w-8/12 h-min items-center m-auto grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-12 lg:gap-x-8">
-            <article className="aspect-h-3 aspect-w-2 overflow-hidden rounded-lg bg-gray-100 sm:col-span-4 lg:col-span-5">
+          <article className="grid w-8/12 h-min items-center m-auto grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-12 lg:gap-x-8">
+            <div className="aspect-h-3 aspect-w-2 overflow-hidden rounded-lg bg-gray-100 sm:col-span-4 lg:col-span-5">
               <img src={`http://localhost:9000/images/${product.image}`} alt="" className="object-cover object-center"/>
-            </article>
+            </div>
             <div className="sm:col-span-8 lg:col-span-7">
-              <h2 className="text-2xl font-bold text-gray-900 sm:pr-12">{product.name}</h2>
-              <div aria-labelledby="information-heading" className="mt-2">
-                <h3 id="information-heading">{product.description}</h3>
-                <p className="text-2xl text-gray-900">${product.price}</p>
+              <div aria-labelledby="information-heading" className="">
+                <h2 className="text-2xl font-bold text-gray-900 sm:pr-12 py-2">{product.name}</h2>
+                <p id="information-heading">{product.description}</p>
+                <p className="text-2xl text-gray-900 py-2">${product.price}</p>
               </div>
-              <article aria-labelledby="options-heading" className="mt-10">
-                  <div>
-                    <h4 className="text-sm font-medium text-gray-900">Color</h4>
-                    <fieldset className="mt-4">
-                      <legend className="sr-only">Choose a color</legend>
-                      <span className="flex items-center space-x-3">
-                        <label className="relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 focus:outline-none ring-gray-400">
-                          <input type="radio" name="color-choice" value="White" className="sr-only" aria-labelledby="color-choice-0-label"/>
-                          <span id="color-choice-0-label" className="sr-only">White</span>
-                          <span aria-hidden="true" className="h-8 w-8 bg-white rounded-full border border-black border-opacity-10"></span>
-                        </label>
-                        <label className="relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 focus:outline-none ring-gray-400">
-                          <input type="radio" name="color-choice" value="Gray" className="sr-only" aria-labelledby="color-choice-1-label"/>
-                          <span id="color-choice-1-label" className="sr-only">Gray</span>
-                          <span aria-hidden="true" className="h-8 w-8 bg-gray-200 rounded-full border border-black border-opacity-10"></span>
-                        </label>
-                        <label className="relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 focus:outline-none ring-gray-900">
-                          <input type="radio" name="color-choice" value="Black" className="sr-only" aria-labelledby="color-choice-2-label"/>
-                          <span id="color-choice-2-label" className="sr-only">Black</span>
-                          <span aria-hidden="true" className="h-8 w-8 bg-gray-900 rounded-full border border-black border-opacity-10"></span>
-                        </label>
-                      </span>
-                    </fieldset>
-                  </div>
-                <h3 id="options-heading" className="text-black">Product options</h3>
-                {product.properties.map((item,index) => {
-                  const [value] = Object.entries(item)
-                  return (
-                    <li key={index}><span className="text-gray-500">{value[0]}</span> : <span>{value[1]}</span></li>
-                    )
-                  })}
-                <p className="text-green-900">The product is available!</p>
+              <div aria-labelledby="options-heading" className="mb-2">
+                <h4 className="text-lg font-medium text-gray-900 py-2">Color</h4>
+                <div className="flex items-center space-x-3">
+                  {product.color.map((item,index) => (
+                    <p aria-hidden="true" key={index} className={`h-8 w-8 bg-${item} rounded-full border border-black border-opacity-10 cursor-pointer`}></p>
+                  ))}
+                </div>
+                </div>
+                <div className="mb-3">
+                  <h3 id="options-heading" className="text-lg text-black py-2">Product options</h3>
+                  <ul>
+                    {product.properties.map((item,index) => {
+                      const [value] = Object.entries(item)
+                      return (
+                        <li key={index}><span className="text-gray-500">{value[0]}</span> : <span>{value[1]}</span></li>
+                        )
+                    })}
+                  </ul>
+                  <p className="text-green-900 py-4">The product is available!</p>
+                </div>
                 <button className="mt-1 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2" onClick={() => handleAddToCart(product)}>Add To Cart</button>
                 <Link to="/" className="mt-1 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Back To Shop</Link>
-              </article>
             </div>
-          </section>
+          </article>
+          <AdditionalDetails showAdditionalDetails={showAdditionalDetails} setShowAdditionalDetails={setShowAdditionalDetails} productExtraDescription={product.extra_description}/>
+          <TechnicalDetails showTechnicalDeatils={showTechnicalDeatils} setShowTechnicalDeatils={setShowTechnicalDeatils} productSpecification={product.specification}/>
         </>
       ) : (
         <div>Loading...</div>
